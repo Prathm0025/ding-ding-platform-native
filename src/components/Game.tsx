e4b31a696be2c981b08c48d0fd355727680d02e5
rana-fullstackdev <ranasingh@Rana-ka-Mac-mini-2.local>
2025-02-26 15:15:17 +0530

    FIX: attempt#6

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,20 +4,27 @@ import { WebView } from 'react-native-webview';
 const GameScreen = () => {
   const webViewRef = useRef(null);
 
+  // Inject JavaScript to send authentication token to Unity WebGL
   const injectedJavaScript = `
-    window.postMessage(${JSON.stringify({ authToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MTIzNDhlY2FhMTdhNzAyYTFjYTJhYyIsInVzZXJuYW1lIjoiUmFuYSIsInJvbGUiOiJwbGF5ZXIiLCJpYXQiOjE3NDA0NTk2MTIsImV4cCI6MTc0MTA2NDQxMn0.FPmCkyzcJQQzc_bwb6K_WZ9CYEC374GI97ffUAEFmeE",socketUrl: "https://game-crm-rtp-backend.onrender.com" })}, '*');
+    window.dispatchEvent(new MessageEvent('message', {
+      data: {
+        type: 'authToken',
+        cookie: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MTIzNDhlY2FhMTdhNzAyYTFjYTJhYyIsInVzZXJuYW1lIjoiUmFuYSIsInJvbGUiOiJwbGF5ZXIiLCJpYXQiOjE3NDA0NTk2MTIsImV4cCI6MTc0MTA2NDQxMn0.FPmCkyzcJQQzc_bwb6K_WZ9CYEC374GI97ffUAEFmeE",
+        socketURL: "https://game-crm-rtp-backend.onrender.com"
+      }
+    }));
     true;
   `;
 
   return (
     <WebView
       ref={webViewRef}
-      source={{ uri: 'https://slot-vikings-dev-sage.vercel.app/' }}
+      source={{ uri: 'https://5944-49-205-44-46.ngrok-free.app/' }}
       injectedJavaScript={injectedJavaScript}
       onMessage={(event) => {
-        console.log('Message from Unity:', event.nativeEvent.data);
+        console.log('Message from Unity:', event);
       }}
-      style={{ width: '90%', height: '80%' }}
+      style={{ width: '100%', height: '100%' }}
     />
   );
 };
