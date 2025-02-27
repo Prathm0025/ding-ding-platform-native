8b976b56e52182f692466dc06419d67a234c901f
rana-fullstackdev <ranasingh@Rana-ka-Mac-mini-2.local>
2025-02-27 16:48:34 +0530

    CODE FORMAT

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,57 +2,61 @@ import axios from "axios";
 import { config } from "../utils/config";
 import * as SecureStore from 'expo-secure-store';
 
-export const loginUser= async(username:string, password:string)=>{
-try {
-    
-    const response = await axios.post(`${config.server}/api/users/login`, {username, password});
-    const data = response.data; 
-    const token = data.token;  
-    await saveToken(token);
-} catch (error:any) {
-    console.log(error.message);
-}
-}
-
-
-
-export const saveToken = async (token:string)=>{
+//function to login user
+export const loginUser = async (username: string, password: string) => {
     try {
-        
-       await SecureStore.setItemAsync('userToken', token, {
-        keychainAccessible:SecureStore.WHEN_UNLOCKED
-       });
+
+        const response = await axios.post(`${config.server}/api/users/login`, { username, password });
+        const data = response.data;
+        const token = data.token;
+       //save the token in secureStore after login
+        await saveToken(token);
+        return data;
+    } catch (error: any) {
+        console.log(error.message);
+    }
+}
+
+
+//function to save token in superStore
+export const saveToken = async (token: string) => {
+    try {
+     
+        await SecureStore.setItemAsync('userToken', token, {
+           // only accessible when device in unlocked
+            keychainAccessible: SecureStore.WHEN_UNLOCKED
+        });
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
 
-export const getAuthToken = async ()=>{
-try{
-    const token = await SecureStore.getItemAsync('userToken');
-    if(token){
-     console.log("Token from Secure Store", token);
-     return token;
-    }else{
-        console.log("No Token found");
-        
+//function to get token from superStore
+export const getAuthToken = async () => {
+    try {
+        const token = await SecureStore.getItemAsync('userToken');
+        if (token) {
+            // console.log("Token from Secure Store", token);
+            return token;
+        } else {
+            console.log("No Token found");
+        }
+    } catch (error) {
+        console.log(error);
+
     }
-}catch(error){
-    console.log(error);
-    
-}
 }
 
+//function to remove token from superStore
+export const removeToken = async () => {
+    try {
+        await SecureStore.deleteItemAsync('userToken');
+        // console.log("Token Removed SuccessFully");
 
-export const removeToken = async ()=>{
-    try{
-    await SecureStore.deleteItemAsync('userToken');
-    console.log("Token Removed SuccessFully");
-    
-    }catch(error){
-        console.log(error);   
+    } catch (error) {
+        console.log(error);
     }
 }
 
