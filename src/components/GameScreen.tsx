04b0b111e59c7ba37e3a9b003a8643ee5eae314a
rohit <rohit@underpinservices.com>
2025-03-04 10:22:09 +0530

    FEAT: loader attempt#3

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,36 +1,22 @@
 import { useRouter } from 'expo-router';
-import React, { useRef, useMemo, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const GameScreen = () => {
-  const webViewRef = useRef(null);
+  const gameWebViewRef = useRef(null);
+  const loaderWebViewRef = useRef(null);
   const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NzkxOTI3OTgxYmU0MzlkMjkyYjg4OSIsInVzZXJuYW1lIjoiTWVodWwiLCJyb2xlIjoicGxheWVyIiwiaWF0IjoxNzQwNjQwODA2LCJleHAiOjE3NDEyNDU2MDZ9.mE6MFKJzX_gvnALAiTmXBKk7sXmt0YwEb8oiMhq2hDU";
   const socketURL = "https://gl9r1h24-5002.inc1.devtunnels.ms/";
   const baseGameUrl = "https://slot-cleopatra-dev.vercel.app/";
-  const loaderUrl = "https://loader.dingdinghouse.com"
-
+  const loaderUrl = "https://loader.dingdinghouse.com";
   const [isGameReady, setIsGameReady] = useState(false);
-
-  const router = useRouter()
-
-  // Encode the auth token and socket URL
-  // const encodedToken = encodeURIComponent(authToken);
-  // const encodedSocketURL = encodeURIComponent(socketURL);
-
-
+  const router = useRouter();
 
   return (
-    <>
-      {isGameReady ? null : (
-        <WebView
-          ref={webViewRef}
-          source={{ uri: loaderUrl }}
-          style={styles.loader}
-        />
-      )}
+    <View style={styles.container}>
       <WebView
-        ref={webViewRef}
+        ref={gameWebViewRef}
         source={{ uri: baseGameUrl }}
         injectedJavaScriptObject={{
           socketURL,
@@ -42,33 +28,43 @@ const GameScreen = () => {
           if (event.nativeEvent.data === 'onExit') {
             router.replace("/home");
           }
-        }}
-        onLoadEnd={() => {
-          setIsGameReady(true);
+          else if (event.nativeEvent.data === 'authToken') {
+            setIsGameReady(true);
+          }
         }}
         style={styles.game}
       />
 
-    </>
+      {!isGameReady && (
+        <WebView
+          ref={loaderWebViewRef}
+          source={{ uri: loaderUrl }}
+          style={styles.loader}
+        />
+      )}
+    </View>
   );
 };
 
 export default GameScreen;
 
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
   loader: {
-    zIndex: 1,
     position: 'absolute',
     top: 0,
     left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    zIndex: 10, // Increased zIndex to ensure loader appears on top
   },
   game: {
     width: '100%',
     height: '100%',
-    zIndex: 0,
-    position: 'absolute',
-    top: 0,
-    left: 0,
+    zIndex: 1,
   }
 });
